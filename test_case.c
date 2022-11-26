dc51c58198cb6b3ac4bbb2b39d23ec627fa07b85
marksikaundi <emmanuelmark117@gmail.com>
2022-11-26 23:54:28 +0200

    test case

--- test_case.c
+++ test_case.c
@@ -1,1 +1,11 @@
-hello team
+#include <stdio.h>
+
+int main() {
+  int sum1 = 100 + 50;        // 150 (100 + 50)
+  int sum2 = sum1 + 250;      // 400 (150 + 250)
+  int sum3 = sum2 + sum2;     // 800 (400 + 400)
+  printf("%d\n", sum1);
+  printf("%d\n", sum2);
+  printf("%d\n", sum3);
+  return 0;
+}
